2e3d1ed3e52f5986b1d36a5512eb6e50e96ca5e0
Alin Roman <163751051+alnrmn@users.noreply.github.com>
2024-08-21 19:59:04 +0300

    Delete pages directory

--- pages/base_page.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-from browser import Browser
-
-
-class BasePage(Browser):
-
-    def get_element_by_link_text(self, text):
-        return self.driver.find_element(By.LINK_TEXT, text)
-
